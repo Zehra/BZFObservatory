af4d8a38cfce90f38b9eff587c33fd3dc54632ae
Zehra <Zehra@users.noreply.github.com>
2026-02-02 12:16:59 -0500

    rewrite observatory.py
    cleanup of code

--- observatory.py
+++ observatory.py
@@ -3,133 +3,100 @@ import sys
 import time
 import requests
 
-# Originally two scripts known as watcher and observer.
-
-# Currently has a friends list, a favorites server and a blocklist.
-# If you don't specify, it tries defaults and if that fails, it just notifies of new player sessions.
-
 if __name__ == '__main__':
-    url = "http://bzstats.strayer.de/stuff/LePoulpe303.php"
-    favoritesinc = True
-    friendsinc = True
-
-    try:
-        friendsfile = open("friends.txt", "r")
-    except:
-        friendsinc = False
-        print("Friends file not found.")
-    
-    try:
-        favoritesfile = open("favorites.txt", "r")
-    except:
-        favoritesinc = False
-        print("Favorite servers file not found.")
-    
-    if friendsinc == True:
-        friends = []
-        friendslist = friendsfile.read()
-        friendsc = -1
-        
-        for friend in friendslist.splitlines():
-            friendsc += 1
-            friends.append(friend)
-        
-        friendsc = len(friends)
-        friendsfile.close()
-
-    if favoritesinc == True:
-        favorites = []
-        favoriteslist = favoritesfile.read()
-        favoritesc = -1
-        
-        for favorite in favoriteslist.splitlines():
-            favoritesc += 1
-            favorites.append(favorite)
-
-        favoritesc = len(favorites)
-        favoritesfile.close()
-
-    if favoritesinc == True and friendsinc == True:
-        phaseset = 1
-    elif favoritesinc == False and friendsinc == True:
-        phaseset = 2
-    elif favoritesinc == True and friendsinc == False:
-        phaseset = 3
+  if len(sys.argv) != 2:
+    sys.exit('Usage: {0} <conf.txt>'.format(sys.argv[0]))
+  try:    
+    conf = open(sys.argv[1], "r")
+  except:
+    sys.exit('Unable to open: {}'.format(sys.argv[1]))
+  # Standard values:
+  url = "http://bzstats.strayer.de/stuff/LePoulpe303.php"
+  favorites=[]
+  friends=[]
+  Display=[]
+  count = 1
+  launch = 0
+  Friend=0
+  Server=0
+  AddToList=-1
+  # Reading and setting configs.
+  confval = conf.read()
+  for line in confval.splitlines():
+    if len(line) >= 7: # [S] Space Username or Server
+      if line[:5] == "[F] ":
+        friends.append(line[5:].lower())
+      if line[:5] == "[S] ":
+        favorites.append(line[5:].lower())
+  conf.close()
+  # Setting standard type
+  if len(favorites) >= 1:
+    count += 1
+  if len(friends) >= 1:
+    count += 1
+  if count == 3:
+    count = 4
+  # Main loop.
+  while True: 
+    req = requests.get(url)
+    if req.status_code != 200:
+      print("ERROR in getting request.")
     else:
-        phaseset = 5
-    phase = phaseset
-    lines = []
-    while True:
-        req = requests.get(url)
-        
-        if req.status_code != 200:
-            print("ERROR in getting request.")
-            time.sleep(60)
-        else:
-            if friendsinc == False and favoritesinc == False:
-                for line in resp.splitlines():
-                    data = line.split("\t")
-                    print("{} ::: On :: {} As {} team".format(data[0], data[2], data[1]))
+      resp = req.text
+      lines = resp.splitlines()
+      if len(resp) < 3:
+        print("===NO PLAYERS FOUND===")
+      elif count == 1:
+        for line in lines:
+          data = line.split("\t")
+          print("{} :: {} :: On :: {}".format(data[0], data[1], data[2]))
+      else:
+        for con in range(count):
+          launch = con
+          for line in lines:
+            AddToList=-1
+            data = line.split("\t")
+            Friend=0
+            Server=0
+            if len(friends) >= 1:
+              for fri in friends:
+                if fri == data[0].lower():
+                  Friend=1
+            if len(favorites) >= 1:
+              for fav in favorites:
+                if fav == data[2].lower():
+                  Server=1
+
+            # Probably the values should be stored in an array or something to lookup.
+            if Friend == 0 and Server == 0:
+              if con == 1 and count == 2 or con == 3 and count == 4:
+                AddToList=3
             else:
-                resp = req.text
-                lines = resp.splitlines()
-                lenlines = len(lines)
-                count = 0
-                phase = phaseset
-                standard = True
-                ruset = 0
-                naset = 0
-                t1 = False
-                t2 = False
-                while phase != 5:
-                    data = lines[count].split("\t")
-                    if favoritesinc == True:
-                        while ruset < favoritesc:
-                            t1 = data[2] == favorites[ruset]
-                            if t1 == True:
-                                break
-                            ruset += 1
-                    if friendsinc == True:  
-                        while naset < friendsc:
-                            t2 = data[0] == friends[naset]
-                            if t2 == True:
-                                break
-                            naset += 1
-                    if phase == 1:
-                        if t1 == True and t2 == True:
-                            if standard == True:
-                                print("===Friends on Favorite Servers:===")
-                                standard = False
-                            print("   {} ::: On :: {} As {} team".format(data[0], data[2], data[1]))
-                    elif phase == 2:
-                        if t1 == False and t2 == True:
-                            if standard == True:
-                                print("===Friends found on Servers:===")
-                                standard = False
-                            print("   {} ::: On :: {} As {} team".format(data[0], data[2], data[1]))
-                    elif phase == 3:
-                        if t1 == True and t2 == False:
-                            if standard == True:
-                                print("===Favorite Servers:===")
-                                standard = False
-                            print("   {} ::: On :: {} As {} team".format(data[0], data[2], data[1]))                 
-                    else:
-                        if friendsinc == True or favoritesinc == True:
-                            if t1 == False and t2 == False:
-                                if standard == True:
-                                    print("===End of favorites/friends===")
-                                    standard = False
-                                print("   {} ::: On :: {} As {} team".format(data[0], data[2], data[1]))
-                    count += 1
-                    if count == lenlines:
-                        count = 0
-                        phase += 1
-                        standard = True
-                        
-                    ruset = 0
-                    naset = 0
-                    t1 = False
-                    t2 = False
-        time.sleep(60 * 5)
-        os.system("clear")
-        # You can use it to run a ping too.
+              if Friend == 1:
+                if Server == 1:# Friend & Server
+                  AddToList = 0
+                else:
+                  if con == 0 and count == 2 or con == 1 and count == 4:# Friend
+                    AddToList=1
+              else: # Server
+                if con == 0 and count == 2 or con == 2 and count == 4:
+                  AddToList=2
+
+            if AddToList != -1:
+              if launch == con:
+                if AddToList == 0:
+                  Display.append("===Friends on Favorite Servers:===")
+                if AddToList == 1:
+                  Display.append("===Friends found on Servers:===")
+                if AddToList == 2:
+                  Display.append("===Favorite Servers:===")
+                if AddToList == 3:
+                  if len(Display) >= 1:
+                    Display.append("===End of favorites/friends===")
+                launch += 1
+              Display.append("   {} ::: On :: {} As {} team".format(data[0], data[2], data[1]))
+        for display in Display:
+          print(display)
+        Display.clear()
+    time.sleep(150) 
+    os.system("clear")
